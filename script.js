0797025677f298cf1a7c319ec6fbf560a2a009e9
josephtchess <90585514+josephtchess@users.noreply.github.com>
2023-03-25 19:53:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 
+// mouse
+const mouse = {
+    x: 10,
+    y: 10,
+    width: 0.1,
+    height: 0.1,
+}
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(e){
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function(){
+    mouse.x = undefined;
+    mouse.y = undefined;
+})
+
 //game board
 const controlsBar = {
     width: canvas.width,
@@ -21,8 +38,10 @@ class Cell {
         this.height = cellSize;
     }
     draw(){
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        if (mouse.x && mouse.y && collision(this, mouse)){
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
     }
 }
 function createGrid(){
@@ -38,15 +57,28 @@ function handleGameGrid(){
         gameGrid[index].draw();
     }
 }
+
 //projectiles
 //defenders
 //enemies
 //recourses
 //utilities
 function animate(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
     handleGameGrid();
     requestAnimationFrame(animate);
 }
 animate();
+
+function collision(first, second){
+    if (    !(  first.x > second.x + second.width ||
+                first.x + first.width < second.x ||
+                first.y > second.y + second.height ||
+                first.y + first.height < second.y)
+    ) {
+        return true;
+    };
+    
+};
