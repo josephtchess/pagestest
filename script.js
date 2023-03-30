c03c3e67e7d2ef1c7f0308525a80b1102ed7c217
josephtchess <90585514+josephtchess@users.noreply.github.com>
2023-03-30 12:01:17 -0400

    Added units and enemies

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
-
+const units = [];
+const enemies = [];
+const enemyVert = [];
+let money = 300;
+let frame = 0;
+let interval = 600;
+let endGame = false;
 // mouse
 const mouse = {
     x: 10,
@@ -60,15 +66,106 @@ function handleGameGrid(){
 
 //projectiles
 //defenders
+class Unit {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '30px Arial';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+    }
+}
+canvas.addEventListener('click', function(){
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if (gridPositionY < cellSize) return;
+    for (let i=0; i < units.length; i++){
+        if(units[i].x == gridPositionX && units[i].y == gridPositionY) return;
+    }
+    let UnitCost = 100;
+    if(money >= UnitCost){
+        units.push(new Unit(gridPositionX,gridPositionY))
+        money -= UnitCost
+    }
+});
+function handleUnits(){
+    for (let i=0; i < units.length; i++){
+        units[i].draw();
+    }
+}
+
 //enemies
+class Enemy {
+    constructor(vert){
+        this.x = canvas.width;
+        this.y = vert;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.health = 100;
+        this.timer = 0;
+        this.speed = Math.random()* 0.2 + 4;
+        this.movement = this.speed;
+        this.maxHealth = this.health;
+    }
+    update(){
+        this.x -= this.movement;
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '30px Arial';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+    }
+}
+function handleEnemies(){
+    for (let i=0; i < enemies.length; i++){
+        enemies[i].update();
+        enemies[i].draw();
+        if (enemies[i].x < 0){
+            endGame = true;
+        }
+    }
+    if (frame % interval == 0){
+        let vert = Math.floor(Math.random() * 5 + 1) * cellSize;
+        enemies.push(new Enemy(vert));
+        enemyVert.push(vert);
+        if (interval > 200) interval -= 50;
+    }
+}
 //recourses
 //utilities
+function handleGameStatus(){
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px Arial';
+    ctx.fillText('Resources ' + money, 20, 55);
+    if (endGame){
+        ctx.fillStyle = 'black';
+        ctx.font = '60px Arial';
+        ctx.fillText('GAME OVER', 135, 330);
+    }
+}
+
 function animate(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
     handleGameGrid();
-    requestAnimationFrame(animate);
+    handleUnits();
+    handleEnemies();
+    handleGameStatus();
+    frame++;
+    if (!endGame) requestAnimationFrame(animate);
 }
 animate();
 
